2c94d4718bd5726e9763d0126cef204e33219b10
imac_1 <imac_1@imac-1.local>
2018-09-28 18:33:27 +0500

    set base settings

--- lib/amazon_advertising_api_ruby.rb
+++ lib/amazon_advertising_api_ruby.rb
@@ -1,5 +1,33 @@
 require "amazon_advertising_api_ruby/version"
 
 module AmazonAdvertisingApiRuby
-  # Your code goes here...
+  TOKEN_URL = "https://api.amazon.com"
+  API_URL = "https://advertising-api.amazon.com"
+  TEST_API_URL = "https://advertising-api-test.amazon.com"
+  EU_API_URL = "https://advertising-api-eu.amazon.com"
+
+  def self.client
+    return OAuth2::Client.new(
+      "",
+      "",
+      :site => TOKEN_URL
+    )
+  end
+
+  # By default this gem will use the production API url unless the test_env module
+  # variable is set to true. Then the test API url will be used
+  def self.active_api_url
+    if test_env
+      return TEST_API_URL
+    end
+    if eu_env
+      return EU_API_URL
+    end
+
+    return API_URL
+  end
+
+  class << self
+    attr_accessor :client_secret, :client_id, :refresh_token, :profile_id, :test_env, :eu_env, :initializing_time
+  end
 end
