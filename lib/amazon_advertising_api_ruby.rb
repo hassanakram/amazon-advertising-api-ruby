53e248faccbb015646bdf8ace1d12e138cfce92a
Mac <mac@Farhan-MacBook-Pro.local>
2018-09-28 17:47:29 +0500

    intial commit

---
+++ lib/amazon_advertising_api_ruby.rb
@@ -0,0 +1,5 @@
+require "amazon_advertising_api_ruby/version"
+
+module AmazonAdvertisingApiRuby
+  # Your code goes here...
+end
---
+++ lib/amazon_advertising_api_ruby/version.rb
@@ -0,0 +1,3 @@
+module AmazonAdvertisingApiRuby
+  VERSION = "0.1.0"
+end
