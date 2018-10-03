5e8fd90943c01a409a59ba98e5428ced58a15e50
imac_1 <imac_1@qumberqbatch.local>
2018-10-03 20:12:15 +0500

    add test and function of authorize in token

--- spec/amazon_advertising_api_ruby/token_spec.rb
+++ spec/amazon_advertising_api_ruby/token_spec.rb
@@ -3,8 +3,12 @@ require "spec_helper"
 RSpec.describe AmazonAdvertisingApiRuby::Token do
   include_context "shared setup"
 
-  it "gets a token" do
-    expect(AmazonAdvertisingApiRuby.access_token).not_to be nil
+  it "gets a token by refresh token" do
+    expect(AmazonAdvertisingApiRuby::Token.retrieve).not_to be nil
   end
 
+  it "returns tokens by " do
+    auth_code = "ANKxXSARRipBoHxnqymk"
+    expect(AmazonAdvertisingApiRuby::Token.authorize(auth_code)["refresh_token"]).not_to be nil
+  end
 end
